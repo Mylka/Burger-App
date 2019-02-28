c5e15a2e9273d204eca9093eae5746b91d8e836f
Mary Em <lmary155@gmail.com>
2019-02-28 12:10:15 +0100

    fetch orders

--- src/components/Navigation/Navigationitems/Navigationitem/Navigationitem.js
+++ src/components/Navigation/Navigationitems/Navigationitem/Navigationitem.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import classes from './Navigationitem.css';
+import { NavLink } from 'react-router-dom';
 
 
 const naviagtionItem = (props) => (
     <li className={classes.NavigationItem}>
-     <a
-      href={props.link}
-      className={props.active ? classes.active : null}> {props.children}</a></li>
+        <NavLink to={props.link} exact activeClassName={classes.active}> {props.children}</NavLink></li>
 );
 
 export default naviagtionItem;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 
-import Order from '../../components/Order/Order';
+import Order from '../../components/Order/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
 
-        render() {
-            return (
-                <div>
-                    <Order/>
-                    <Order/>
-                </div>
-            ) 
-        }
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                let fetchedOrders = []
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                }
+                this.setState({ loading: false, orders: fetchedOrders })
+            })
+            .catch(err => {
+                this.setState({ loading: false })
+            })
 
     }
 
-export default Orders;
+    render() {
+        return (
+            <div>
+                <Order />
+                <Order />
+            </div>
+        )
+    }
+
+}
+
+export default withErrorHandler(Orders, axios);

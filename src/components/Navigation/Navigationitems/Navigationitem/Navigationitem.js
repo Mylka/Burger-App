62cd0b934e9e01e77bb0232433ad9944a4d2c780
Mary Em <lmary155@gmail.com>
2019-03-01 10:24:52 +0100

    dynamically create inputs inside the form

--- src/components/Navigation/Navigationitems/Navigationitem/Navigationitem.js
+++ src/components/Navigation/Navigationitems/Navigationitem/Navigationitem.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 
 const naviagtionItem = (props) => (
     <li className={classes.NavigationItem}>
-        <NavLink to={props.link} exact activeClassName={classes.active}> {props.children}</NavLink></li>
+        <NavLink to={props.link} exact={props.exact} activeClassName={classes.active}> {props.children}</NavLink></li>
 );
 
 export default naviagtionItem;
--- src/components/Navigation/Navigationitems/Navigationitems.js
+++ src/components/Navigation/Navigationitems/Navigationitems.js
@@ -4,7 +4,7 @@ import NavigationItem from './Navigationitem/Navigationitem';
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/"> Burger Builder</NavigationItem>
+        <NavigationItem link="/" exact> Burger Builder</NavigationItem>
         <NavigationItem link="/orders"> Orders</NavigationItem>
     </ul>
 );

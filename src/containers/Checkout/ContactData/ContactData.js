93de50335fd9c82339c64d101d84b71d26cce809
Mary Em <lmary155@gmail.com>
2019-03-02 11:45:25 +0100

    add validation feedback

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -21,7 +21,8 @@ class ContactData extends Component {
                 validation: {
                     required: true,
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -33,7 +34,8 @@ class ContactData extends Component {
                 validation: {
                     required: true,
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -47,7 +49,8 @@ class ContactData extends Component {
                     minLength: 5,
                     maxLength: 5
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: 'input',
@@ -59,7 +62,8 @@ class ContactData extends Component {
                 validation: {
                     required: true,
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: 'input',
@@ -71,7 +75,8 @@ class ContactData extends Component {
                 validation: {
                     required: true,
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -91,13 +96,13 @@ class ContactData extends Component {
         let isValid = true;
         
         if(rules.required) {
-            isValid = value.trim() !== '' && isValid;
+            isValid = value.trim() !== '' && isValid
         }
         if(rules.minLength){
-            isValid = value >= rules.minLength && isValid
+            isValid = value.length >= rules.minLength && isValid
         }
         if(rules.maxLength){
-            isValid = value <= rules.maxLength && isValid
+            isValid = value.length <= rules.maxLength && isValid
         }
 
 
@@ -138,9 +143,10 @@ class ContactData extends Component {
             ...updatedOrderForm[inputIdentifier]
         };
         updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-
+        console.log(updatedFormElement)
         this.setState({ orderForm: updatedOrderForm })
     }
 
@@ -162,6 +168,9 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        shouldValidate={formElement.config.validation}
+                        invalid={!formElement.config.valid}
+                        touched={formElement.config.touched}
                         changed={(event) => this.inputChangeHandler(event, formElement.id)} />
                 ))}
                 <Button buttonType="Success">ORDER</Button>
